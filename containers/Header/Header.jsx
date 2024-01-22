5070e1a319e66613ab83b282317728496adfa75c
Ahtasham ul haq <ahtasham1802@gmail.com>
2024-01-22 17:41:15 +0500

    commit

--- containers/Header/Header.jsx
+++ containers/Header/Header.jsx
@@ -1,9 +1,9 @@
-"use client"
+"use client";
 
 import React from "react";
 import { motion } from "framer-motion";
 import "./Header.css";
-import { AppWrap } from '../../wrapper'
+import { AppWrap } from "../../wrapper";
 import Link from "next/link";
 import Image from "next/image";
 
@@ -13,17 +13,16 @@ const scaleVariants = {
     opacity: [0, 1],
     transition: {
       duration: 1,
-      ease: 'easeInOut'
-    }
-  }
-}
+      ease: "easeInOut",
+    },
+  },
+};
 
 const Header = () => {
-
   return (
     <div className="app__header app__flex">
-        {/* <div className="absolute w-12 h-12 bg-gradient-to-tr from-slate-400 to-white left-32 bottom-36 rounded-full" /> */}
-        {/* <div className="absolute w-48 h-48 bg-gradient-to-tl from-slate-500 to-white/75 right-12 top-24 dark:hidden rounded-full" /> */}
+      {/* <div className="absolute w-12 h-12 bg-gradient-to-tr from-slate-400 to-white left-32 bottom-36 rounded-full" /> */}
+      {/* <div className="absolute w-48 h-48 bg-gradient-to-tl from-slate-500 to-white/75 right-12 top-24 dark:hidden rounded-full" /> */}
       <motion.div
         whileInView={{ x: [-100, 0], opacity: [0, 1] }}
         transition={{ duration: 0.5 }}
@@ -34,16 +33,29 @@ const Header = () => {
             <span>👋</span>
             <div style={{ marginLeft: 20 }}>
               <p className="p-text dark:text-white">Hello, I am</p>
-              <h1 className="text-[2.75rem] font-medium text-center text-black-color dark:text-white font-abril">Ahtasham</h1>
+              <h1 className="text-[2.75rem] font-medium text-center text-black-color dark:text-white font-abril">
+                Ahtasham
+              </h1>
             </div>
           </div>
           <div className="tag-cmp app__flex dark:bg-light-dark-bg">
             <p className="p-text dark:text-white">Web Developer</p>
             <p className="p-text dark:text-white">Web Designer</p>
           </div>
-          <Link href={'mailto:ahtasham1802@gmail.com'} className="min-w-[290px] transition duration-300 box-shadow-white shadow-lg lg:mt-24 mt-12 flex flex-row justify-start items-center my-4 p-4 rounded-[10px] cursor-pointer bg-secondary-color dark:bg-light-dark-bg">
-            <img src="/email.png" alt="email" width={40} height={40} className="mx-[0.7rem]" />
-            <p className="text-[0.8rem] text-white">ahtasham1802@gmail.com</p>
+          <Link
+            href={"mailto:ahtasham1802@gmail.com"}
+            className="min-w-[290px] transition duration-300 box-shadow-white shadow-lg lg:mt-24 mt-12 flex flex-row justify-start items-center my-4 p-4 rounded-[10px] cursor-pointer  bg-light-dark-bg/30 dark:bg-light-dark-bg"
+          >
+            <img
+              src="/email.png"
+              alt="email"
+              width={40}
+              height={40}
+              className="mx-[0.7rem]"
+            />
+            <p className="text-[0.8rem] dark:text-white">
+              ahtasham1802@gmail.com
+            </p>
           </Link>
         </div>
       </motion.div>
@@ -52,12 +64,12 @@ const Header = () => {
         transition={{ duration: 0.5, delay: 0.5 }}
         className="app__header-img"
       >
-        <Image src={'/profile.png'} fill={true} priority={true} alt="profile-bg" />
+        {/* <img src={'/profile.png'} alt="profile-bg" className="profile_img" /> */}
         <motion.img
           whileInView={{ scale: [0, 1] }}
-          transition={{ duration: 1, ease: 'easeInOut' }}
-          className="overlay_circle"
-          src={'/circle.svg'}
+          transition={{ duration: 1, ease: "easeInOut" }}
+          className="overlay_circle mix-blend-soft-light"
+          src={"/circle.svg"}
           alt="profile_circle"
         />
       </motion.div>
@@ -66,19 +78,18 @@ const Header = () => {
         whileInView={scaleVariants.whileInView}
         className="app__header-circles"
       >
-          <div className="circle-cmp app__flex dark:bg-light-dark-bg">
-            <Image src={"/figma.png"} width={60} height={60} alt="circle" />
-          </div>
-          <div className="circle-cmp app__flex dark:bg-light-dark-bg">
-            <Image src={"/next.png"} width={90} height={90} alt="circle" />
-          </div>
-          <div className="circle-cmp app__flex dark:bg-light-dark-bg">
-            <Image src={"/python.png"} width={42} height={42} alt="circle" />
-          </div>
+        <div className="circle-cmp app__flex dark:bg-light-dark-bg">
+          <Image src={"/figma.png"} width={60} height={60} alt="circle" />
+        </div>
+        <div className="circle-cmp app__flex dark:bg-light-dark-bg">
+          <Image src={"/next.png"} width={90} height={90} alt="circle" />
+        </div>
+        <div className="circle-cmp app__flex dark:bg-light-dark-bg">
+          <Image src={"/python.png"} width={42} height={42} alt="circle" />
+        </div>
       </motion.div>
-
     </div>
-  )
+  );
 };
 
-export default AppWrap(Header, 'home');
+export default AppWrap(Header, "home");
--- containers/Navbar/Navbar.jsx
+++ containers/Navbar/Navbar.jsx
@@ -12,11 +12,11 @@ import ThemeSwitch from "@/components/ThemeSwitcher";
 const Navbar = () => {
   const [toggle, setToggle] = useState(false)
   return (
-    <nav className="app__navbar">
+    <nav className="app__navbar shadow-lg">
       <div className="app__navbar-logo">
         <a href="/">
-        {/* <img src={'/portfolio.png'} alt="logo" /> */}
-        <p className="text-3xl font-abril text-secondary-color dark:text-yellow-bg">Portfolio<span className="dark:text-white">.</span></p>
+          {/* <img src={'/portfolio.png'} alt="logo" /> */}
+          <p className="text-3xl font-abril text-secondary-color dark:text-yellow-bg">Portfolio<span className="dark:text-white">.</span></p>
         </a>
       </div>
       <ul className="app__navbar-links">
@@ -28,28 +28,28 @@ const Navbar = () => {
         ))}
       </ul>
       <div className="flex items-center gap-4">
-          <ThemeSwitch />
-          <SocialMedia />
+        <ThemeSwitch />
+        <SocialMedia />
       </div>
       <div className="app__navbar-menu">
         <HiMenuAlt4 onClick={() => setToggle(true)} />
 
         {toggle && (
           <>
-          <motion.div
-            whileInView={{ x: [300, 0] }}
-            transition={{ duration: .85, ease: 'easeOut' }}
-            className=""
-          >
-            <HiX onClick={() => setToggle(false)} />
-            <ul >
-              {["home", "about", "work"].map((item) => (
-                <li key={item}>
-                  <a href={`#${item}`} onClick={() => setToggle(false)} >{item}</a>
-                </li>
-              ))}
-            </ul>
-          </motion.div>
+            <motion.div
+              whileInView={{ x: [300, 0] }}
+              transition={{ duration: .85, ease: 'easeOut' }}
+              className=""
+            >
+              <HiX onClick={() => setToggle(false)} />
+              <ul >
+                {["home", "about", "work"].map((item) => (
+                  <li key={item}>
+                    <a href={`#${item}`} onClick={() => setToggle(false)} >{item}</a>
+                  </li>
+                ))}
+              </ul>
+            </motion.div>
           </>
         )}
       </div>
