a865bc0358272314ae4b0152e96433dc6df01eb0
Ahtasham ul-Haq <ahtasham1802@gmail.com>
2024-05-24 01:23:59 +0500

    about-section-completed

--- app/page.tsx
+++ app/page.tsx
@@ -1,13 +1,12 @@
+import Grid from "@/components/Grid";
 import Hero from "@/components/Hero";
-import { FloatingNav } from "@/components/ui/FloatingNav";
-import Image from "next/image";
 
 export default function Home() {
   return (
     <main className="relative bg-black-100 flex justify-center items-center flex-col overflow-hidden mx-auto sm:px-10 px-5">
       <div className="max-w-7xl w-full">
         <Hero />
-        
+        <Grid />
       </div>
     </main>
   );
